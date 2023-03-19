d613f9349e3c61e0c8000dda837485af8ebc9f23
Andrew Dassonville <dassonville.andrew@gmail.com>
2023-03-19 00:36:44 -0700

    Add more plane functionality

--- src/components/Plane/Map/Map.js
+++ src/components/Plane/Map/Map.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import { LayersControl, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import classes from './Map.module.scss';
+import L from 'leaflet';
+import React, { useEffect, useMemo } from 'react';
+import { FeatureGroup, LayersControl, MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import airports from '../data/us_airports.json';
+import classes from './Map.module.scss';
 
 const kcvo = airports.find(airport => airport.ICAO === 'KCVO');
 
-const Map = () => {
+const PathLayer = (props) => {
+    const map = useMap();
+
+    const { departureAirport, arrivalAirport } = props;
+
+    const departure = useMemo(() => airports.find(airport => airport.ICAO === departureAirport), [departureAirport]);
+    const arrival = useMemo(() => airports.find(airport => airport.ICAO === arrivalAirport), [arrivalAirport]);
+
+    useEffect(() => {
+        if (departureAirport && arrivalAirport) {
+            const departure = airports.find(airport => airport.ICAO === departureAirport);
+            const arrival = airports.find(airport => airport.ICAO === arrivalAirport);
+
+            if (!departure || !arrival) return;
+
+            const departurePoint = L.latLng(departure.lat, departure.lon);
+            const arrivalPoint = L.latLng(arrival.lat, arrival.lon);
+
+            const path = L.polyline([departurePoint, arrivalPoint], { color: 'magenta', weight: 8 });
+            path.addTo(map);
+
+            map.fitBounds([
+                [departure.lat, departure.lon],
+                [arrival.lat, arrival.lon]
+            ], { padding: [25, 25] });
+
+            return () => {
+                map.removeLayer(path);
+            }
+        }
+    }, [map, departureAirport, arrivalAirport]);
+
+    return (
+        <FeatureGroup>
+            {[departure, arrival].filter(i => i).map(airport => (
+                <Marker key={airport.ICAO} position={[Number(airport.lat), Number(airport.lon)]}>
+                    <Popup>
+                        {airport.ICAO}: {airport.name}
+                    </Popup>
+                </Marker>
+            ))}
+        </FeatureGroup>
+    )
+}
+
+const Map = (props) => {
+    const { departureAirport, arrivalAirport } = props;
+
     const cycle = '20230223'
     const center = [Number(kcvo.lat), Number(kcvo.lon)];
 
@@ -13,6 +61,9 @@ const Map = () => {
         <>
             <MapContainer center={center} zoom={8} maxZoom={11} className={classes.aviationMap}>
                 <LayersControl position="topright">
+
+                    {/* Map Layers */}
+
                     <LayersControl.BaseLayer name="Hybrid VFR" checked>
                         <TileLayer url={`https://vfrmap.com/${cycle}/tiles/vfrc/{z}/{y}/{x}.jpg`} tms={true} />
                     </LayersControl.BaseLayer>
@@ -33,7 +84,9 @@ const Map = () => {
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                     </LayersControl.BaseLayer>
 
-                    <LayersControl.Overlay name="Radar">
+                    {/* Weather Layers */}
+
+                    <LayersControl.Overlay name="Radar" checked>
                         <TileLayer url="https://a.sat.owm.io/maps/2.0/radar/{z}/{x}/{y}?appid=9de243494c0b295cca9337e1e96b00e2&day=2023-03-19T04:00" />
                     </LayersControl.Overlay>
 
@@ -41,7 +94,7 @@ const Map = () => {
                         <TileLayer url="https://a.sat.owm.io/vane/2.0/weather/CL/{z}/{x}/{y}?appid=9de243494c0b295cca9337e1e96b00e2" />
                     </LayersControl.Overlay>
 
-                    <LayersControl.Overlay name="Wind">
+                    <LayersControl.Overlay name="Wind" checked>
                         <TileLayer url="https://a.sat.owm.io/vane/2.0/weather/WND/{z}/{x}/{y}?appid=9de243494c0b295cca9337e1e96b00e2&fill_bound=true" />
                     </LayersControl.Overlay>
 
@@ -50,13 +103,17 @@ const Map = () => {
                     </LayersControl.Overlay>
                 </LayersControl>
 
-                {airports.map(airport => (
-                    <Marker key={airport.ICAO} position={[Number(airport.lat), Number(airport.lon)]}>
-                        <Popup>
-                            {airport.ICAO}: {airport.name}
-                        </Popup>
-                    </Marker>
-                ))}
+                <PathLayer departureAirport={departureAirport} arrivalAirport={arrivalAirport} />
+
+                {/* <FeatureGroup>
+                    {airports.map(airport => (
+                        <Marker key={airport.ICAO} position={[Number(airport.lat), Number(airport.lon)]}>
+                            <Popup>
+                                {airport.ICAO}: {airport.name}
+                            </Popup>
+                        </Marker>
+                    ))}
+                </FeatureGroup> */}
             </MapContainer>
         </>
     )
--- src/components/Plane/Plane.js
+++ src/components/Plane/Plane.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Map from './Map/Map';
 import classes from './Plane.module.scss';
 import { Helmet } from 'react-helmet';
 
 const Plane = () => {
+    const [departureAirport, setDepartureAirport] = useState('')
+    const [arrivalAirport, setArrivalAirport] = useState('')
+
     return (
         <>
             <Helmet>
@@ -13,9 +16,11 @@ const Plane = () => {
 
             <div className={classes.planeContainer}>
                 <div className={classes.planeRouteForm}>
-
+                    <input className={classes.planeAirportInput} placeholder="Departure airport" value={departureAirport} onChange={e => setDepartureAirport(e.target.value?.toUpperCase())} />
+                    <input className={classes.planeAirportInput} placeholder="Arrival airport" value={arrivalAirport} onChange={e => setArrivalAirport(e.target.value?.toUpperCase())} />
                 </div>
-                <Map />
+
+                <Map departureAirport={departureAirport} arrivalAirport={arrivalAirport} />
             </div>
         </>
     )
